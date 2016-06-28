2a130e0383ad13e8a9978c45c545fb07d4bd22db
Dinh Luu <dinhluu@server.local>
2016-06-28 11:12:07 +0700

    add pan gesture, collectionView delegate: moveItemAtIndexPath

--- DLNewsCollectionView/DLNewsCollectionView/DLNewsCollectionViewLayout.swift
+++ DLNewsCollectionView/DLNewsCollectionView/DLNewsCollectionViewLayout.swift
@@ -10,5 +10,61 @@ import UIKit
 
 class DLNewsCollectionViewLayout: UICollectionViewLayout {
 
+  let itemGap: CGFloat = 10.0
+  let itemSize: CGFloat = 250.0
+  let numVisibleItems = 4
+  var yOrigin: CGFloat = 200.0
+  let xOrigin: CGFloat = 20.0
+  var scale: CGFloat = 0.9
+  let visibleItem = 3
+  var attributes = [UICollectionViewLayoutAttributes]()
   
+  override func prepareLayout() {
+    super.prepareLayout()
+    if attributes.count > 0 {
+      return
+    }
+    
+    let numItems = collectionView!.numberOfItemsInSection(0)
+    var transform = CATransform3DIdentity
+    transform.m34 = -1/5000
+    for index in 0..<numItems {
+      let indexPath = NSIndexPath(forItem: index, inSection: 0)
+      let attribute = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+      attribute.frame = CGRect(x: collectionView!.center.x - itemSize/2, y: yOrigin, width: itemSize, height: itemSize)
+      transform = CATransform3DScale(CATransform3DIdentity, scale, scale , 1)
+      attribute.transform3D = transform
+      attribute.zIndex = 0 - index
+      yOrigin -= 20.0
+      scale -= 0.1
+      
+      if index > visibleItem {
+        attribute.hidden = true
+      }
+      
+      attributes.append(attribute)
+    }
+  }
+  
+  override func collectionViewContentSize() -> CGSize {
+    return CGSize(width: collectionView!.bounds.width, height: collectionView!.bounds.height)
+  }
+  
+  override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+    for attr in attributes {
+      attr.zIndex = 0 - attr.indexPath.item
+    }
+    return attributes
+  }
+  
+  override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+    let attri = attributes[indexPath.item]
+    attri.transform3D = CATransform3DTranslate(attri.transform3D, 0, 0, -CGFloat(attri.indexPath.item))
+    return attributes[indexPath.item]
+  }
+  
+  override func layoutAttributesForInteractivelyMovingItemAtIndexPath(indexPath: NSIndexPath, withTargetPosition position: CGPoint) -> UICollectionViewLayoutAttributes {
+    return super.layoutAttributesForInteractivelyMovingItemAtIndexPath(indexPath, withTargetPosition: position)
+  }
+
 }
--- DLNewsCollectionView/DLNewsCollectionView/DLNewsController.swift
+++ DLNewsCollectionView/DLNewsCollectionView/DLNewsController.swift
@@ -13,6 +13,7 @@ class DLNewsController: UIViewController {
   var collectionView: UICollectionView! = nil
   var collectionViewLayout: DLNewsCollectionViewLayout! = nil
   var dataSource = [UIColor]()
+  var isInteracting = false
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,11 +22,15 @@ class DLNewsController: UIViewController {
     collectionView.translatesAutoresizingMaskIntoConstraints = false
     collectionView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
     
-    //collectionView.delegate = self
+    collectionView.delegate = self
     collectionView.dataSource = self
+    //collectionView.userInteractionEnabled = true
     
     collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "NewsCell")
     view.addSubview(collectionView)
+    
+    let pullGesture = UIPanGestureRecognizer(target: self, action: #selector(self.interactive(_:)))
+    collectionView.addGestureRecognizer(pullGesture)
     
     initDatasoruce()
   }
@@ -43,6 +48,26 @@ class DLNewsController: UIViewController {
     return (cor1: cor1, cor2: cor2, cor3: cor3)
   }
   
+  @objc func interactive(gesture: UIPanGestureRecognizer) {
+    let position = gesture.locationInView(view)
+    guard let indexPath = collectionView.indexPathForItemAtPoint(position) else {return}
+    
+    switch gesture.state {
+    case .Began:
+      if isInteracting { return }
+      
+      isInteracting = true
+      collectionView.beginInteractiveMovementForItemAtIndexPath(indexPath)
+    case .Changed:
+      collectionView.updateInteractiveMovementTargetPosition(position)
+    case .Cancelled:
+      isInteracting = false
+      collectionView.cancelInteractiveMovement()
+    default:
+      isInteracting = false
+      collectionView.endInteractiveMovement()
+    }
+  }
 }
 
 extension DLNewsController: UICollectionViewDataSource {
@@ -54,5 +79,17 @@ extension DLNewsController: UICollectionViewDataSource {
     let cell = collectionView.dequeueReusableCellWithReuseIdentifier("NewsCell", forIndexPath: indexPath)
     cell.backgroundColor = dataSource[indexPath.row]
     return cell
+  }
+}
+
+extension DLNewsController: UICollectionViewDelegate {
+  func collectionView(collectionView: UICollectionView, targetIndexPathForMoveFromItemAtIndexPath originalIndexPath: NSIndexPath, toProposedIndexPath proposedIndexPath: NSIndexPath) -> NSIndexPath {
+    return NSIndexPath(forItem: 0, inSection: 0)
+  }
+  
+  func collectionView(collectionView: UICollectionView, moveItemAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
+    print("source: \(sourceIndexPath.row)")
+    print("destination: \(destinationIndexPath.row)")
+    //dataSource.removeAtIndex(sourceIndexPath.row)
   }
 }
